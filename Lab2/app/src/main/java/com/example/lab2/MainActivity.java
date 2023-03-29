db3328ae8c7e9c0c2b56e772280e0b88817522bb
Thái Tăng Đức <thaitangduc1@gmail.com>
2023-03-29 21:56:00 +0700

    Emp

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etName, etSalary;
+    private Button btnCalculate;
+    private ListView lvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        etSalary = findViewById(R.id.etSalary);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        lvResult = findViewById(R.id.lvResult);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                double salary = Double.parseDouble(etSalary.getText().toString());
+                double totalSalary = salary * 2; // Tính lương = lương cơ bản * 2
+                String result = name + ": " + totalSalary; // Chuỗi kết quả
+
+                // Hiển thị kết quả lên ListView
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(R.layout.list_view, new String[]{result});
+                lvResult.setAdapter(adapter);
+            }
+        });
     }
 }
