942e754a527ef06ceb11b58a28f15032edf8e8b0
Thái Tăng Đức <thaitangduc1@gmail.com>
2023-04-12 00:09:00 +0700

    Fix

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,71 +1,59 @@
 package com.example.lab2;
-import com.example.lab2.Employee;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.example.payroll.Staff;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etName, etSalary;
-    private Button btnCalculate;
-    private ListView lvResult;
-    private EmployeeListAdapter employees;
 
-    private List<Employee> EmployeeList;
+    EditText etName;
+    EditText etSalary;
+    Button btnCac;
+    ListView listStaff;
+
+    String name;
+    String crossSalary ;
+
+    ArrayList<Staff> arrayList = new ArrayList<>();
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etName = findViewById(R.id.etName);
-        etSalary = findViewById(R.id.etSalary);
-        btnCalculate = findViewById(R.id.btnCalculate);
-        lvResult = findViewById(R.id.lvResult);
-//        employees = new EmployeeListAdapter(this, R.layout, employees);
-        lvResult.setAdapter(employees);
+        etName = findViewById(R.id.ET_name);
+        etSalary = findViewById(R.id.ET_salary);
+        btnCac = findViewById(R.id.btn_Cac);
 
 
-        btnCalculate.setOnClickListener(new View.OnClickListener() {
+
+        listStaff = findViewById(R.id.list_Staff);
+
+        btnCac.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String name = etName.getText().toString();
-                int salary = Integer.parseInt(etSalary.getText().toString());
+            public void onClick(View view) {
 
-//                Employee employee = new Employee(name, salary);
-//                employees.add(employee);
-//
-//                ArrayAdapter<Employee> adapter = new ArrayAdapter<Employee>(MainActivity.this, android.R.layout.simple_list_item_1, employees);
-//
-//                lvResult.setAdapter(adapter);
-//                lvResult.deferNotifyDataSetChanged();
-//
-//                if (!name.isEmpty() && !salary.isEmpty()) {
-//                    double salary = Double.parseDouble(salaryString);
-//                    Employee employee = new Employee(name, salary);
-//                    mEmployeeList.add(employee);
-//                    mEmployeeListAdapter.notifyDataSetChanged();
-//
-//                    etName.setText("");
-//                    etSalary.setText("");
-//                } else {
-//                    Toast.makeText(MainActivity.this, "Hãy nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
-//                }
-//                double salary = Double.parseDouble(etSalary.getText().toString());
-//                double totalSalary = salary * 2; // Tính lương = lương cơ bản * 2
-//                String result = name + ": " + totalSalary; // Chuỗi kết quả
+                name = String.valueOf(etName.getText());
 
-                // Hiển thị kết quả lên ListView
-//                ArrayAdapter<String> adapter = new ArrayAdapter<>(R.layout.list_view, new String[]{result});
-//                lvResult.setAdapter(adapter);
+                //Add công thức tính net
+                crossSalary = String.valueOf(etSalary.getText());
+
+                com.example.payroll.Staff staff = new Staff(name, crossSalary);
+                arrayList.add(staff);
+
+                adapter = new Adapter(MainActivity.this, R.layout.item_listview, arrayList);
+                listStaff.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
             }
         });
     }
---
+++ Lab2/app/src/main/java/com/example/lab2/Staff.java
@@ -0,0 +1,27 @@
+package com.example.payroll;
+
+public class Staff {
+    private String name;
+    private String salary;
+
+    public Staff(String name, String salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
